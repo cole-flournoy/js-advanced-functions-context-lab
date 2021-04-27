4f37d7e53a23f6ce15d42ab6bd02505dfb9a5d93
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-27 16:35:42 -0400

    commit tree bc279797689c7d644aad2fcb89d874ef8b72af6b

--- index.js
+++ index.js
@@ -8,8 +8,69 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ 
+ let createEmployeeRecord = employeeArray => {
+    return {
+      firstName: employeeArray[0],
+      familyName: employeeArray[1],
+      title: employeeArray[2],
+      payPerHour: employeeArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
 
-let allWagesFor = function () {
+  let createEmployeeRecords = emplArrays => {
+    return emplArrays.map(r => createEmployeeRecord(r))
+  }
+  
+ let createTimeInEvent = function(dateTimeString){
+    let [date, time] = dateTimeString.split(" ")
+
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(time),
+      date: date
+    })
+    return this
+  }
+  
+let createTimeOutEvent = function(dateTimeString){
+    let [date, time] = dateTimeString.split(" ")
+
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(time),
+      date: date
+    })
+    return this
+  }
+  
+  let hoursWorkedOnDate = function(dateString){
+    const outEvent = this.timeOutEvents.find(e => e.date === dateString)
+    const inEvent = this.timeInEvents.find(e => e.date === dateString)
+    return (outEvent.hour - inEvent.hour)/ 100
+  }
+  
+  let wagesEarnedOnDate = function(dateString){
+    const hours = hoursWorkedOnDate.call(this, dateString)
+    return hours * this.payPerHour
+  }
+  
+
+  let findEmployeeByFirstName = (srcArray, firstName) => {
+    return srcArray.find(record => record.firstName === firstName)
+  }
+  
+  let calculatePayroll = empRecords => {
+    return empRecords.reduce((total, currentRecord) => total + allWagesFor.call(currentRecord), 0)
+  }
+  
+
+
+
+
+ let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
